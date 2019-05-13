78dbb66ab61dde781d9f81e7c735899116681b1e
Anbu <anbazhagan.planis@envrmnt.com>
2019-05-13 07:29:23 -0400

    Adding favicons

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -6,30 +6,7 @@ class Landing extends Component {
     return (
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
-          <div className="col s12 center-align">
-            <Link
-              to="/register"
-              style={{
-                width: "140px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px"
-              }}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Register
-            </Link>
-            <Link
-              to="/login"
-              style={{
-                width: "140px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px"
-              }}
-              className="btn btn-large btn-flat waves-effect white black-text"
-            >
-              Log In
-            </Link>
-          </div>
+          <div className="col s12 center-align">Recipes goes here</div>
         </div>
       </div>
     );
